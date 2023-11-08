23d5125a87a69c8178489706166720f9be7230d1
Rafael <22-36209@g.batstate-u.edu.ph>
2023-11-08 12:42:53 +0800

    feat/create-table-appointments

--- veterinary.sql
+++ veterinary.sql
@@ -10,3 +10,10 @@ CREATE TABLE owners(
 	email VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE appointments(
+	appointid INT PRIMARY KEY,
+	appointdate DATE,
+	reason VARCHAR(255) NOT NULL,
+	animalid INT,
+	foreign key (animalid) references animals(animalid)
+);
