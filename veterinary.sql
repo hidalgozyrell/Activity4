6c0906b73de670872617b528df8f3bf2fd42c52e
nachtchydazz093 <22-36209@g.batstate-u.edu.ph>
2023-11-08 13:19:49 +0800

    feat/insert-10-rows-doctors

--- veterinary.sql
+++ veterinary.sql
@@ -99,3 +99,13 @@ VALUES
     (8, 'Simba', 'Cat', 'Bengal', '2018-06-18', 'Male', 'Spotted Rosetted', 8),
     (9, 'Max', 'Dog', 'Dachshund', '2020-07-14', 'Male', 'Black and Tan', 9),
     (10, 'Cleo', 'Cat', 'Ragdoll', '2019-12-22', 'Female', 'Seal Point', 10);
+
+    -- Insert doctor data into the Doctors table
+INSERT INTO Doctors (doctorid, dfirstname, dlastname, speciality, phone, email)
+VALUES
+    (1, 'Dr. Maria', 'Santos', 'General Veterinarian', '987-654-3210', 'maria@example.com'),
+    (2, 'Dr. Antonio', 'Gonzales', 'Feline Specialist', '555-123-4567', 'antonio@example.com'),
+    (3, 'Dr. Felipe', 'Luna', 'Orthopedic Specialist', '111-222-3333', 'felipe@example.com'),
+    (4, 'Dr. Sofia', 'Reyes', 'Dermatology Specialist', '999-888-7777', 'sofia@example.com'),
+    (5, 'Dr. Luis', 'Torres', 'Surgery Specialist', '123-555-7777', 'luis@example.com'),
+    (6, 'Dr. Carmen', 'Fernandez', 'Ophthalmology Specialist', '333-222-1111', 'carmen@example.com');
